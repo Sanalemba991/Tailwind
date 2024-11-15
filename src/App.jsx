b31c9488b0c8880aabcdc8885d058938d380320f
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-15 18:39:48 +0530

    making learn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 export default function App() {
   return (
-    <h1 className="text-4xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-4xl font-bold underline text-center text-blue-800 ">
+        Hello world!
+      </h1>
+
+      <div className="text-center bg-violet-200 h-90 m-11 w-full  rounded   ">
+        <p className=" font-serif  text-3xl mt-4 ">sama </p>
+      </div>
+    </>
+  );
 }
