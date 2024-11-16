14ed5f77b851de73368b014f71c1d52cbe39e653
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-16 19:40:39 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -32,10 +32,33 @@ export default function App() {
         </button>
         <ul className="mx-2 my-2 space-y-2">
           <li className="bg-white p-2 first:bg-yellow-100">item1</li>
-          <li className="bg-white p-2 first:bg-yellow-400 odd:bg-blue-300 even:bg-green-500">item 2</li>
-          <li className="bg-white p-2 first:bg-yellow-400 odd:bg-blue-300 even:bg-green-500">item 3</li>
-          <li className="bg-white p-2 first:bg-yellow-400 odd:bg-blue-300 even:bg-green-500">item 4</li>
+          <li className="bg-white p-2 first:bg-yellow-400 odd:bg-blue-300 even:bg-green-500">
+            item 2
+          </li>
+          <li className="bg-white p-2 first:bg-yellow-400 odd:bg-blue-300 even:bg-green-500">
+            item 3
+          </li>
+          <li className="bg-white p-2 first:bg-yellow-400 odd:bg-blue-300 even:bg-green-500">
+            item 4
+          </li>
         </ul>
+        <div className="m-10 rounded-lg bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5">
+          <h3 className="text-base  font-medium tracking-tight  text-slate-900">
+            this is text element
+          </h3>
+          <p className="mt-2 text-small  text-slate-500">
+            This is even longer p tag
+          </p>
+          <button
+            id="toggleDark"
+            className="text-black-900 px-4 py-2 text-s font-medium mt-8 bg-blue-300 rounded-xl"
+            onClick="document.body.classList.toggle('dark')"
+          >
+            Dark mode
+          </button>
+        </div>
+<div>  <p className="text-2xl bg-stone-200 p-[16px]">chest not color</p></div>
+      
       </main>
     </>
   );
