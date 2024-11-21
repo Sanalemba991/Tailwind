54f51e5962cf948ad0cb1570e6ade2f032bd174b
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-21 23:48:25 +0530

    tick added

---
+++ src/component/Tick.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Tick() {
+  return (
+    <div className=" ml-96 mt-16   ">
+        <div className="text-white">
+      <ul>
+        <li ><span className="font-bold mr-1">190</span>Channels (including local and sports)</li>
+        <li> <span className="font-bold">Free</span> voice  Remote & DVR included</li>
+        <li> Includeds access to over 80000 On demand title</li>
+        <li></li>
+      </ul>
+    </div>
+    </div>
+  );
+}
+
+export default Tick;
